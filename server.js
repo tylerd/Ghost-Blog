52c166222a4a16f481ae248370ae79ebd296ae73
Tyler Doerksen <tylergd@outlook.com>
2016-07-14 13:48:03 -0500

    Updating Ghost Version

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var ghost = require('ghost');
 var path = require('path');
 var appInsights = require('applicationinsights')
 
-console.log('App Insights Key: ' + process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
 
-appInsights.setup().start();
+if(process.env.APPINSIGHTS_INSTRUMENTATIONKEY) { 
+    console.log('App Insights Key: ' + process.env.APPINSIGHTS_INSTRUMENTATIONKEY);
+    appInsights.setup().start();
+}
+else { 
+    console.log('APPINSIGHTS_INSTRUMENTATIONKEY is not set. Skipping appinsights startup'); 
+}
 
 ghost({
   config: path.join(__dirname, 'config.js')
