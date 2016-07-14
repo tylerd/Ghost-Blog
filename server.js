b816a5e86e476a2bc688d125638d3696cd12507e
Tyler Doerksen <tylergd@outlook.com>
2016-07-14 11:40:45 -0500

    Adding Application Insights

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var ghost = require('ghost');
 var path = require('path');
+var appInsights = require('applicationinsights')
 
+console.log('App Insights Key: ' + process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+
+appInsights.setup().start();
 
 ghost({
   config: path.join(__dirname, 'config.js')
